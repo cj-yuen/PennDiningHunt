f89e0a1c9c45c446c1709cc0532ca9b9bd0d3e1d
kevinwd2401 <109395354+kevinwd2401@users.noreply.github.com>
2024-10-28 19:20:24 -0400

    Initial commit

---
+++ PennDiningHunt/PennDiningHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennDiningHuntApp.swift
+//  PennDiningHunt
+//
+//  Created by Kevin D on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PennDiningHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
